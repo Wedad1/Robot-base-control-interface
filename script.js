0495e2276abd9eb1db9b9549293883c2a763c64d
Wedad Alkhathami <71224889+Wedad1@users.noreply.github.com>
2021-06-26 19:45:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+    var sl = document.getElementById("Range");
+    var op = document.getElementById("S1");
+    op.innerHTML = sl.value; 
+    sl.oninput = function() {
+      op.innerHTML = this.value
+    }
+    var sl2 = document.getElementById("Range2");
+    var op2 = document.getElementById("S2");
+    op2.innerHTML = sl2.value; 
+    sl2.oninput = function() {
+      op2.innerHTML = this.value
+    }
+    var sl3 = document.getElementById("Range3");
+    var op3 = document.getElementById("S3");
+    op3.innerHTML = sl3.value; 
+    sl3.oninput = function() {
+      op3.innerHTML = this.value
+    }
+    var sl4 = document.getElementById("Range4");
+    var op4 = document.getElementById("S4");
+    op4.innerHTML = sl4.value; 
+    sl4.oninput = function() {
+      op4.innerHTML = this.value
+    }
+    var sl5 = document.getElementById("Range5");
+    var op5 = document.getElementById("S5");
+    op5.innerHTML = sl5.value; 
+    sl5.oninput = function() {
+      op5.innerHTML = this.value
+    }
+    var sl6 = document.getElementById("Range6");
+    var op6 = document.getElementById("S6");
+    op6.innerHTML = sl6.value; 
+    sl6.oninput = function() {
+      op6.innerHTML = this.value
+    }
+    
